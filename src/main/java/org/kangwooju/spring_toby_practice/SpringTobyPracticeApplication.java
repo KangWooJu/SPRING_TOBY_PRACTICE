d943e58e60f1155a121c4f646fc29dd934c0c798
KangWooJu <space1102@naver.com>
2025-04-14 13:20:52 +0900

    feat:dafault package upload

---
+++ src/main/java/org/kangwooju/spring_toby_practice/SpringTobyPracticeApplication.java
@@ -0,0 +1,13 @@
+package org.kangwooju.spring_toby_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTobyPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTobyPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kangwooju/spring_toby_practice/SpringTobyPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kangwooju.spring_toby_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTobyPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
