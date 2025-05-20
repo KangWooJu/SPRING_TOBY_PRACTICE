b4a4dd05ff920df77a3a2639ba3f630c5009840f
KangWooJu <space1102@naver.com>
2025-05-20 16:04:25 +0900

    fix : User 엔티티의 enum 타입 필드결정 메소드의 접근 지정자 변경

--- src/main/java/org/kangwooju/spring_toby_practice/domain/user/Entity/Level.java
+++ src/main/java/org/kangwooju/spring_toby_practice/domain/user/Entity/Level.java
@@ -2,19 +2,26 @@ package org.kangwooju.spring_toby_practice.domain.user.Entity;
 
 import lombok.AllArgsConstructor;
 
-@AllArgsConstructor
 public enum Level {
 
-    GOLD(3),SILVER(2),BRONZE(1);
+    GOLD(3), SILVER(2), BRONZE(1);
 
     private final int value;
 
-    private static Level valueOf(int value){
-        switch (value){
+    private Level(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public static Level fromValue(int value) {
+        switch (value) {
             case 3: return GOLD;
             case 2: return SILVER;
             case 1: return BRONZE;
+            default: throw new IllegalArgumentException("Unknown value: " + value);
         }
     }
-
 }
