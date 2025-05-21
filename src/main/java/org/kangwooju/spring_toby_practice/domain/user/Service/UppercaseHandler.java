eacfd6a800aeed914e63ca6811e120a00190898f
KangWooJu <space1102@naver.com>
2025-05-21 22:35:10 +0900

    feat : Dynamic Proxy 도입을 위한 Handler 클래스 생성

---
+++ src/main/java/org/kangwooju/spring_toby_practice/domain/user/Service/UppercaseHandler.java
@@ -0,0 +1,19 @@
+package org.kangwooju.spring_toby_practice.domain.user.Service;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class UppercaseHandler implements InvocationHandler {
+
+    private Hello target;
+
+    public UppercaseHandler(Hello target){
+        this.target = target;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        String ret = (String)method.invoke(target,args);
+        return ret.toUpperCase();
+    }
+}
